ad0a5c073983d97869df8faf99ecf74b7fbe73da
acatarinaoaraujo <acoa_@hotmail.com>
2021-08-08 11:16:08 -1000

    edit profile #7

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -28,7 +28,7 @@ class NavBar extends React.Component {
           {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
             <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
           ) : ''}
-          <Menu.Item>
+          <Menu.Item position="right">
             {this.props.currentUser === '' ? (
               <Dropdown id="login-dropdown" text="Login" pointing="top right" icon={'user'}>
                 <Dropdown.Menu>
@@ -39,6 +39,8 @@ class NavBar extends React.Component {
             ) : (
               <Dropdown id="navbar-current-user" text={this.props.currentUser} pointing="top right" icon={'user'}>
                 <Dropdown.Menu>
+                  {/* eslint-disable-next-line max-len */}
+                  <Dropdown.Item icon="user" text="My Profile" as={NavLink} exact to="/profile" />
                   <Dropdown.Item id="navbar-sign-out" icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
                 </Dropdown.Menu>
               </Dropdown>
--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,36 +1,35 @@
 import React from 'react';
-import { Grid, Segment, Header, Form, Loader } from 'semantic-ui-react';
-import { AutoForm, TextField, LongTextField, SubmitField } from 'uniforms-semantic';
+import { Card, Container, Header, Loader, Image, Grid, Segment, Form, Divider, Icon } from 'semantic-ui-react';
+import { AutoForm, SubmitField, TextField, LongTextField } from 'uniforms-semantic';
 import swal from 'sweetalert';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
 import { Meteor } from 'meteor/meteor';
-import { _ } from 'meteor/underscore';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-import { updateProfileMethod } from '../../startup/both/Method';
 import { Profiles } from '../../api/profile/Profiles';
 
 /** Create a schema to specify the structure of the data to appear in the form. */
-const makeSchema = () => new SimpleSchema({
-  email: { type: String, label: 'Email', optional: true },
-  firstName: { type: String, label: 'First', optional: true },
-  lastName: { type: String, label: 'Last', optional: true },
-  bio: { type: String, label: 'Biographical statement', optional: true },
+const formSchema = new SimpleSchema({
+  email: { type: String, label: 'Email', unique: true },
+  firstName: { type: String, label: 'First', optional: false },
+  lastName: { type: String, label: 'Last', optional: false },
+  bio: { type: String, label: 'Bio', optional: true },
   phone: { type: String, label: 'Phone', optional: true },
-  picture: { type: String, label: 'Picture URL', optional: true },
+  picture: { type: String, label: 'Profile Picture', optional: true },
 });
 
-/** Renders the Home Page: what appears after the user logs in. */
+/** Renders the Profile page */
 class Profile extends React.Component {
 
   /** On submit, insert the data. */
   submit(data) {
-    Meteor.call(updateProfileMethod, data, (error) => {
+    const { _id, email, firstName, lastName, bio, picture, phone } = data;
+    Profiles.collection.update(_id, { $set: { email, firstName, lastName, bio, picture, phone } }, (error) => {
       if (error) {
         swal('Error', error.message, 'error');
       } else {
-        swal('Success', 'Profile updated successfully', 'success');
+        swal('Success', 'Profile updated successfully. Refresh to view changes.', 'success');
       }
     });
   }
@@ -40,47 +39,70 @@ class Profile extends React.Component {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
   }
 
-  /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
+  /** Render the page once subscriptions have been received. */
   renderPage() {
     const email = Meteor.user().username;
-    const formSchema = makeSchema();
     const bridge = new SimpleSchema2Bridge(formSchema);
     const profile = Profiles.collection.findOne({ email });
-    const model = _.extend({}, profile);
     return (
-      <Grid id="home-page" container centered>
-        <Grid.Column>
-          <Header as="h2" textAlign="center">Your Profile</Header>
-          <AutoForm model={model} schema={bridge} onSubmit={data => this.submit(data)}>
-            <Segment>
-              <Form.Group widths={'equal'}>
-                <TextField id='firstName' name='firstName' showInlineError={true} placeholder={'First Name'}/>
-                <TextField id='lastName' name='lastName' showInlineError={true} placeholder={'Last Name'}/>
-                <TextField name='email' showInlineError={true} placeholder={'email'} disabled/>
-              </Form.Group>
-              <LongTextField id='bio' name='bio' placeholder='Write a little bit about yourself.'/>
-              <Form.Group widths={'equal'}>
-                <TextField name='phone' showInlineError={true} placeholder={'Phone'}/>
-                <TextField name='picture' showInlineError={true} placeholder={'URL to picture'}/>
-              </Form.Group>
-              <SubmitField id='home-page-submit' value='Update'/>
-            </Segment>
-          </AutoForm>
-        </Grid.Column>
-      </Grid>
+      <Container>
+        <Header style={{ marginTop: '40px' }} as="h4" textAlign="center">Edit My Profile</Header>
+        <Grid divided='vertically'>
+          <Grid.Row>
+            <Grid.Column width={4}>
+              <Card>
+                <Image src={profile.picture} wrapped ui={false} />
+                <Card.Content>
+                  <Card.Header>{profile.firstName} {profile.lastName}</Card.Header>
+                  <Card.Meta>
+                    <Icon name="mail"></Icon>
+                    <span>{email}</span>
+                  </Card.Meta>
+                  <Card.Meta>
+                    <Icon name="phone"></Icon>
+                    <span>{profile.phone}</span>
+                  </Card.Meta>
+                  <Card.Description><em>
+                    {profile.bio}</em>
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+            <Grid.Column width={12}>
+              <Grid.Row>
+                <AutoForm model={profile} schema={bridge} onSubmit={data => this.submit(data)}>
+                  <Segment className='AddForm'>
+                    <Form.Group widths={'equal'}>
+                      <TextField id='firstName' name='firstName' showInlineError={true} placeholder={'First Name'}/>
+                      <TextField id='lastName' name='lastName' showInlineError={true} placeholder={'Last Name'}/>
+                      <TextField name='email' showInlineError={true} placeholder={'Email'} disabled/>
+                    </Form.Group>
+                    <TextField id='picture' name='picture' showInlineError={true} placeholder={'Image URL'}/>
+                    <LongTextField id='bio' name='bio' placeholder='Tell us about yourself.'/>
+                    <TextField id='phone' name='phone' showInlineError={true} placeholder={'Phone'}/>
+                    <SubmitField id='profile-update' value='Update'/>
+                  </Segment>
+                </AutoForm>
+              </Grid.Row>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     );
   }
 }
 
+/** Require an array of Stuff documents in the props. */
 Profile.propTypes = {
+  profile: PropTypes.array,
   ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
-  // Ensure that minimongo is populated with all collections prior to running render().
-  const sub1 = Meteor.subscribe(Profiles.userPublicationName);
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe(Profiles.userPublicationName);
   return {
-    ready: sub1.ready(),
+    ready: subscription.ready(),
   };
 })(Profile);
