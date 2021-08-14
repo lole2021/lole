58b9064352f83aabeb09cab1c40f1f6b7f58c96f
acatarinaoaraujo <acoa_@hotmail.com>
2021-08-13 21:20:13 -1000

    eslint error fix

--- app/imports/ui/components/ClothesCard.jsx
+++ app/imports/ui/components/ClothesCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Card, Image, Header, Icon } from 'semantic-ui-react';
+import { Button, Card, Image, Header } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 
